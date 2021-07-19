402303c9ede6a7b908ee969b1074559e4a6cc842
gabriel <gabyk800@gmail.com>
2021-07-19 21:27:32 +0300

    launch-list implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppComponent } from './app.component';
 import { LaunchListComponent } from './launch-list/launch-list.component';
 import { LaunchDetailsComponent } from './launch-details/launch-details.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LaunchListComponent,
-    LaunchDetailsComponent
-  ],
+  declarations: [AppComponent, LaunchListComponent, LaunchDetailsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/launch-list/launch-list.component.ts
+++ src/app/launch-list/launch-list.component.ts
@@ -1,16 +1,18 @@
+import { PastLaunchesListGQL } from './../services/spacexGraphql.service';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-launch-list',
   templateUrl: './launch-list.component.html',
   styleUrls: ['./launch-list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LaunchListComponent implements OnInit {
+  constructor(private pastLaunchesService: PastLaunchesListGQL) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  pastLaunches$ = this.pastLaunchesService
+    .fetch({ limit: 30 })
+    .pipe(map((res) => res.data.launchesPast));
+  ngOnInit(): void {}
 }
